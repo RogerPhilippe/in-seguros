c42d3948dd1ad6c9ceba96e53a44a23d4d23954f
Roger Philippe <roger.pereira@mc1.com.br>
2020-07-15 21:04:17 -0300

    Implementação crashlytics.

--- app/src/main/java/br/com/inseguros/InSegurosApplication.kt
+++ app/src/main/java/br/com/inseguros/InSegurosApplication.kt
@@ -7,18 +7,29 @@ import br.com.inseguros.di.daoModule
 import br.com.inseguros.di.dbModule
 import br.com.inseguros.di.repositoryModule
 import br.com.inseguros.di.viewModelModules
+import com.crashlytics.android.Crashlytics
+import com.crashlytics.android.core.CrashlyticsCore
+import io.fabric.sdk.android.Fabric
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 
-class MainApplication : Application() {
+
+class InSegurosApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
 
+        Fabric.with(
+            this,
+            Crashlytics.Builder().core(
+                CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build()
+            ).build()
+        )
+
         startKoin {
             androidLogger()
-            androidContext(this@MainApplication)
+            androidContext(this@InSegurosApplication)
             modules(
                 listOf(viewModelModules, dbModule, daoModule, repositoryModule)
             )
